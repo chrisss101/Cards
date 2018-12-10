36ff90fed892e4434dc8242c939f87427f41e466
BT_1N3_19@bktech.local <BT_1N3_19@bktech.local>
2018-12-10 10:11:59 -0500

    Initial commit

---
+++ src/Card.java
@@ -0,0 +1,13 @@
+public class Card {
+    public String rank;
+    public String suit;
+    public int positiveValue;
+    public Card(String rank,String suit,int positiveValue) {
+        this.rank = rank;
+        this.suit = suit;
+        this.positiveValue = positiveValue;
+    }
+    public String toString() {
+
+    }
+}
