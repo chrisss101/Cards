32a1f2899b5d754ac17fde1f3afa951f79811564
chrisss101 <Crafaniello9535@bths.edu>
2018-12-12 10:09:46 -0500

    dgsssssssssss

--- src/Deck.java
+++ src/Deck.java
@@ -2,8 +2,20 @@ import java.util.ArrayList;
 
 public class Deck {
     ArrayList<Card> Dealt;
-    public Deck(Card suit,Card rank,Card point) {
-        Card newCard = new Card(suit,rank,point);
+    ArrayList<Card> unDealt;
+
+    int[] point = new int[6];
+
+    public Deck(String[] suit,String[] rank,int[] point) {
+        Dealt = new ArrayList<Card>();
+        unDealt = new ArrayList<Card>();
+        for (int i = 0; i< suit.length;i++) {
+            for ( int j = 0; j < rank.length;j++) {
+                Card newCard = new Card(rank[j],suit[i],point[j]);
+                unDealt.add(newCard);
+            }
+        }
+
     }
     public boolean isEmpty() {
         if (Dealt.size() == 0) {
@@ -13,4 +25,12 @@ public class Deck {
             return false;
         }
     }
+   public int size() {
+        return unDealt.size();
+   }
+   public Card Deal(Card toDeal) {
+        Dealt.add(toDeal);
+       unDealt.remove(toDeal);
+       return toDeal;
+   }
 }
